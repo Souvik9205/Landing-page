00a52516a8d821e69d8aeb5168b7db74c8e056aa
Souvik Mukhopadhyay <souvik9205@gmail.com>
2024-04-08 17:26:28 +0530

    career error fix

--- src/app/careers/page.jsx
+++ src/app/careers/page.jsx
@@ -7,8 +7,8 @@ function page() {
       <div className="flex w-full ">
         <div className="w-2 sm:w-1/2 flex items-center">
           <h1 className="text-medium sm:text-4xl lg:text-6xl font-medium pl-10 md:pl-36 ">
-            Let's{" "}
-            <span className="text-red-500">make great things happen!</span>
+            Let's
+            <span className="text-red-500"> make great things happen!</span>
           </h1>
         </div>
         <div className="w-full sm:w-1/2">
@@ -17,8 +17,8 @@ function page() {
       </div>
       <div>
         <h1 className="text-5xl font-medium py-10">
-          We're always looking for people who want to make real{" "}
-          <span className="text-red-500">impact.</span>
+          We're always looking for people who want to make real
+          <span className="text-red-500"> impact.</span>
         </h1>
         <p className="text-lg pt-5 pb-3">
           We are a collective of smart people trying to solve real world
@@ -31,8 +31,8 @@ function page() {
           doing it.
         </p>
         <p className="text-lg py-3 pb-10">
-          Do you think we may be a good fit? Get in touch with us through{" "}
-          <span className="text-red-500 underline">careers@pericius.com</span>
+          Do you think we may be a good fit? Get in touch with us through
+          <span className="text-red-500 underline"> careers@pericius.com</span>
         </p>
 
         <h2 className="text-5xl font-medium py-20">
