3e881bd3da04f0f60fa1eea1ebcfe32743f85600
Souvik Mukhopadhyay <souvik9205@gmail.com>
2024-04-09 17:37:45 +0530

    privacy

--- src/app/privacy-policy/page.jsx
+++ src/app/privacy-policy/page.jsx
@@ -1,7 +1,195 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="p-5 sm:p-10 lg:px-36 lg:mx-20">
+      <div className="pb-3">
+        <h1 className="text-4xl sm:text-6xl text-center font-medium sm:px-3 pb-8">
+          PRIVACY POLICY
+        </h1>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">1. Introduction</h2>
+        <p className="text-lg py-2 px-3">
+          This Privacy Policy (the &quot;
+          <span className="font-bold text-lg">Policy</span>&quot;) describes
+          your privacy rights regarding our collection, use, storage, and
+          protection of your personal information while accessing our website
+          located at https://www.pericius.com/ (the &quot;
+          <span className="font-bold text-lg">Site</span>&quot;) and all related
+          applications, tools, service and events regardless of how you access
+          or use them. You accept this Privacy Policy when you access the Site
+          or use any features, technologies, functions or services offered on
+          it. We may amend this Policy at any time by posting a revised version
+          on the Site. The revised version will be effective at the time we post
+          it. This Policy relates to our privacy practices about the Site. We
+          are not responsible for the content or privacy practices of other
+          websites you may access from any link inserted in the Site. For the
+          sake of clarity, any use of the Site automatically comprehends your
+          acceptance of this Policy of Pericius Technologies Private Limited
+          and/or its affiliates (collectively called as &quot;
+          <span className="font-bold text-lg">Pericius</span>&quot;).
+        </p>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">
+          2. How we collect information about you{" "}
+        </h2>
+        <p className="text-lg py-2 px-3">
+          As a visitor, you do not have to submit any personal information to
+          use the Site. This Site only collects personal information that is
+          specifically and voluntarily provided by visitors. Such information
+          may consist of, but is not limited to, your name, address, email
+          address and telephone. Regarding the newsletter subscription, with
+          your consent, your name, email and company name will be properly
+          stored and privately handled, according to the General Data Protection
+          Regulation (GDPR) and the applicable data protection laws. The email
+          address you provide for subscription may be used to send you
+          information and updates pertaining to our services, in addition to
+          receiving occasional Pericius news, updates, related product or
+          service information. If at any time, you would like to unsubscribe
+          from receiving future emails, we include detailed unsubscribe
+          instructions at the bottom of each email.
+        </p>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">
+          3. How we use personal information
+        </h2>
+        <p className="text-lg py-2 px-3">
+          Our primary purpose in collecting personal information is to provide
+          you with a secure and efficient experience. You may provide personal
+          information via this Site, for example, to gain access to specific
+          content, attend a hosted event, respond to a survey, or request
+          communications about specific areas of interest. In such cases, the
+          information you submit will be used to manage your request and to
+          customize and improve this Site and related services offered to you.
+        </p>
+        <p className="text-lg py-2 px-3">
+          Nevertheless, we may use your personal information for marketing
+          purposes, or to send you promotional materials or communications
+          regarding services provided by entities within Pericius that we feel
+          may be of interest to you. If you do not wish to receive marketing
+          related information from us, you may choose to opt out of it by
+          unsubscribing for the same.
+        </p>
+        <p className="text-lg py-2 px-3">
+          Your personal information may also be used to protect our rights or
+          property and that of our users and, where appropriate, to comply with
+          legal process.
+        </p>
+        <p className="text-lg py-2 px-3">
+          This Site collects standard internet log information including your IP
+          address, browser type and language, access times, and referring Site
+          addresses.
+        </p>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">
+          4. Exceptional Circumstances in which we Share Your Information
+        </h2>
+        <p className="text-lg py-2 px-3">
+          Pericius will not share your personal data with third parties under
+          any circumstances, except with government officials, or other third
+          parties pursuant to a court order, or other legal process or
+          requirements applicable to Pericius or one of its clients, suppliers
+          and partners. We need to do so in order to comply with law; or when we
+          believe that the disclosure of personal information is necessary to
+          prevent harm or financial loss, to report suspected illegal activity
+          or to investigate violations of our rights. Pericius will not sell or
+          rent any of your personal information to third parties for their
+          marketing purposes.
+        </p>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">
+          5. How we protect and store personal information
+        </h2>
+        <p className="text-lg py-2 px-3">
+          Throughout this Policy, we use the term &quot;personal
+          information&quot; to describe information that can be associated with
+          a specific person and can be used to identify that person and as
+          prescribed under the GDPR. We do not consider personal information to
+          include information that has been made anonymous so that it does not
+          identify a specific user. We store and process your personal
+          information on our computers and servers. We protect your information
+          using physical, technical, and administrative security measures to
+          reduce the risks of loss, misuse, unauthorized access, disclosure and
+          alteration.
+        </p>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">6. Data Protection Rights</h2>
+        <p className="text-lg py-2 px-3">
+          In order to ensure transparency, we have listed below the following
+          data protection rights which you are entitled to (i) Right to Access,
+          wherein you may request us for copies of your personal data, and in
+          lieu of which we may charge you a small fee; (ii) Right to
+          Rectification, wherein you have the right to request us to rectify
+          your information which you believe is inaccurate; (iii) Right to
+          Erasure, wherein you may request us to erase your personal data, under
+          certain conditions; (iv) Right to Restrict Processing, wherein you
+          have the right to request us to restrict processing your personal
+          information, under certain conditions; (v) Right to Object to
+          Processing, wherein you have the right to object to us processing your
+          personal information, under certain conditions and (vi) Right to Data
+          Portability, wherein you have the right to request us to transfer your
+          data to another company or directly to you, under certain conditions.{" "}
+        </p>
+        <p className="text-lg py-2 px-3">
+          We will revert to any request made by you as mentioned above within 30
+          (thirty) working days from the date of receipt of your request.{" "}
+        </p>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">7. Information Security</h2>
+        <p className="text-lg py-2 px-3">
+          All security measures are consistent with international information
+          practices, to protect your personal information. These measures
+          include technical and procedural steps to protect all data from
+          misuse, unauthorized access, disclosure, loss, alteration, or
+          destruction.
+        </p>
+        <p className="text-lg py-2 px-3">
+          We are fully committed to protect our users from unauthorized access
+          to any information we hold. Specifically:
+        </p>
+        <div>
+          <p className="text-lg py-2 px-3">
+            (i) We encrypt all our services and applications;
+          </p>
+          <p className="text-lg py-2 px-3">
+            (ii) We evaluate our information gathering, storage and processing
+            practices, plus physical security measures, to protect against
+            unauthorized access to Pericius systems; and
+          </p>
+          <p className="text-lg py-2 px-3">
+            (iii) We restrict the access regarding personal information to
+            Pericius employees, contractors and partners, who are subject to
+            strict contractual confidentiality obligations.
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">
+          8. How we answer visitors&apos; questions
+        </h2>
+        <p className="text-lg py-2 px-3">
+          If you have any questions or concerns regarding your privacy while
+          using this Site, please contact us at [insert phone number] and/or
+          [insert email address].
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default page;
