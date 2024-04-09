1654ee048830c1391462da40c996511e3ecbc0bf
Souvik Mukhopadhyay <souvik9205@gmail.com>
2024-04-09 18:00:42 +0530

    cookie

--- src/app/cookie-policy/page.jsx
+++ src/app/cookie-policy/page.jsx
@@ -1,7 +1,132 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="p-5 sm:p-10 lg:px-36 lg:mx-20">
+      <div className="pb-3">
+        <h1 className="text-4xl sm:text-6xl text-center font-medium sm:px-3 pb-8">
+          COOKIE POLICY
+        </h1>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">1. Introduction</h2>
+        <p className="text-lg py-2 px-3">
+          This Cookie Policy (the &quot;
+          <span className="font-bold text-lg">Policy</span>&quot;) describes the
+          standard internet log information and visitor behaviour information
+          which we collect from you when you visit our website located at
+          https://www.pericius.com/ (the &quot;
+          <span className="font-bold text-lg">Site</span>&quot;) and all related
+          applications, tools, service and events regardless of how you access
+          or use them. You accept this Policy when you access the Site or use
+          any features, technologies, functions or services offered on it. We
+          may amend this Policy at any time by posting a revised version on the
+          Site. The revised version will be effective at the time we post it. We
+          are not responsible for the content or practices of other websites you
+          may access from any link inserted in the Site. For the sake of
+          clarity, any use of the Site automatically comprehends your acceptance
+          of this Policy of Pericius Technologies Private Limited and/or its
+          affiliates (collectively called as &quot;
+          <span className="font-bold text-lg">Pericius</span>&quot;).
+        </p>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">2. What are Cookies? </h2>
+        <p className="text-lg py-2 px-3">
+          Cookies are text files placed on your computer to collect standard
+          Internet log information and visitor behaviour information. When you
+          visit our Site, we may collect information from you automatically
+          through cookies or similar technology.
+        </p>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">
+          3. How we Use Cookies and Kinds of Cookies We Use
+        </h2>
+        <p className="text-lg py-2 px-3">
+          We use cookies in a range of ways to improve your experience on our
+          Site, including:
+        </p>
+        <div className="px-20">
+          <ul className="list-disc">
+            <li>
+              <p className="text-lg py-2 px-3">Keeping you signed in;</p>
+            </li>
+            <li>
+              <p className="text-lg py-2 px-3">
+                Understanding how you use our Site
+              </p>
+            </li>
+          </ul>
+        </div>
+        <p className="text-lg py-2 px-3">
+          We use cookies to enable Site functionality, understand the
+          performance of our Site, provide social media features, and serve more
+          relevant content to you. We may also place cookies on our and our
+          partners&apos;s behalf to help us deliver more targeted ads and assess
+          the performance of these campaigns.
+        </p>
+        <p className="text-lg py-2 px-3">
+          If you wish to not have this information used for the purpose of
+          serving you targeted ads, you may opt-out by clicking here [insert
+          hyperlink]. Please note this does not opt you out of being served
+          advertising and that you will continue to receive generic ads.
+        </p>
+        <p className="text-lg py-2 px-3">
+          When you access our Site, we use the following cookies:
+        </p>
+        <div className="px-10">
+          <ul className="list-disc">
+            <li>
+              <p className="text-lg py-2 px-3">
+                <span className="font-bold text-lg">Functionality -</span> We
+                use these so that we recognize you on our Site and remember your
+                previously selected preferences. These could include what
+                language you prefer and location you are in. A mix of
+                first-party and third-party cookies are used
+              </p>
+            </li>
+            <li>
+              <p className="text-lg py-2 px-3">
+                <span className="font-bold text-lg">Advertising -</span> We use
+                these to collect information about your visit to our Site, the
+                content you viewed, the links you followed and information about
+                your browser, device, and your IP address. In certain instance
+                we may share some limited aspects of this data with third
+                parties for advertising purposes. We may also share online data
+                collected through cookies with our advertising partners. This
+                means that when you visit another website, you may be shown
+                advertising based on your browsing patterns on our Site.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">4. How to manage Cookies</h2>
+        <p className="text-lg py-2 px-3">
+          You can set your browser not to accept cookies by clicking on the
+          button prompted upon visiting our Site. However, in a few cases, some
+          of the features of our Site may not function as a result of your
+          rejection to the cookies.
+        </p>
+      </div>
+
+      <div>
+        <h2 className="font-bold py-4 text-lg ">
+          5. How we answer visitors&apos; questions
+        </h2>
+        <p className="text-lg py-2 px-3">
+          If you have any questions or concerns regarding our Cookie Policy
+          while using this Site, please contact us at support@pericius.com.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default page;
